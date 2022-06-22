431da14ee2c7813f4522adae8c2e441ae3195650
Jeffcaco <jeffreycaco98@gmail.com>
2022-06-22 02:22:43 -0500

    version1

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$db = [
+    'host' => 'bqbnbljhuhouxmxfwlq6-mysql.services.clever-cloud.com',
+    'username' => 'ulm2un2gywrdxzyq',
+    'password' => 'tQFP1nMNIAj4G102hSCn',
+    'db' => 'bqbnbljhuhouxmxfwlq6' //Cambiar al nombre de tu base de datos
+];
+
+?>
---
+++ post.php
@@ -0,0 +1,97 @@
+<?php
+include "config.php";
+include "utils.php";
+
+
+$dbConn =  connect($db);
+
+/*
+  listar todos los posts o solo uno
+ */
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+    if (isset($_GET['id']))
+    {
+      //Mostrar un post
+      $sql = $dbConn->prepare("SELECT * FROM Clientes where id=:id");
+      $sql->bindValue(':id', $_GET['id']);
+      $sql->execute();
+      header("HTTP/1.1 200 OK");
+      echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
+      exit();
+	  }
+    else {
+      //Mostrar lista de post
+      $sql = $dbConn->prepare("SELECT * FROM Clientes");
+      $sql->execute();
+      $sql->setFetchMode(PDO::FETCH_ASSOC);
+      header("HTTP/1.1 200 OK");
+      echo json_encode( $sql->fetchAll()  );
+      exit();
+	}
+}
+
+// Crear un nuevo post
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+  $input = json_decode(file_get_contents('php://input'),true);
+    /*$input = $_POST;*/
+    $sql = "INSERT INTO Clientes
+          (user,password,name,phone)
+          VALUES
+          ('".$input['user']."','".$input['password']."','".$input['name']."',".$input['phone'].")";
+    $statement = $dbConn->prepare($sql);
+    bindAllValues($statement, $input);
+    $statement->execute();
+    $postId = $dbConn->lastInsertId();
+    if($postId)
+    {
+      $input['id'] = $postId;
+      header("HTTP/1.1 200 OK");
+      
+      echo json_encode($input);
+      exit();
+	 }
+}
+
+//Borrar
+if ($_SERVER['REQUEST_METHOD'] == 'DELETE')
+{
+	$id = $_GET['id'];
+  $statement = $dbConn->prepare("DELETE FROM Clientes where id=:id");
+  $statement->bindValue(':id', $id);
+  $statement->execute();
+	header("HTTP/1.1 200 OK");
+	exit();
+}
+
+//Actualizar
+if ($_SERVER['REQUEST_METHOD'] == 'PUT')
+{
+    $input = $_GET;
+    $input2 = json_decode(file_get_contents('php://input'),true);
+    $postId = $input['id'];
+    $fields = getParams($input);
+
+    $sql = "
+          UPDATE Clientes
+          SET user='".$input2['user']."',password='"
+              .$input2['password']."',name='"
+              .$input2['name']."',phone="
+              .$input2['phone']."
+          WHERE id=$postId
+           ";
+
+    $statement = $dbConn->prepare($sql);
+    bindAllValues($statement, $input);
+
+    $statement->execute();
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+
+//En caso de que ninguna de las opciones anteriores se haya ejecutado
+header("HTTP/1.1 400 Bad Request");
+
+?>
